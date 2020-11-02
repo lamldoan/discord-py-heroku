3e59aa4df72fed721ebc27746f12269f9b78ec9c
lamldoan <72846047+lamldoan@users.noreply.github.com>
2020-11-02 12:11:20 +1100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,11 @@
 import os
 from discord.ext import commands
 
+import os
+import random
+
+import discord
+
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -11,6 +16,31 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 if __name__ == "__main__":
     bot.run(TOKEN)
