3881a0af031f5b6686d06c5850c5c18ae9629234
lamldoan <72846047+lamldoan@users.noreply.github.com>
2020-11-08 20:23:52 +1100

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,12 @@
 import os
 import random
+import requests
+import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
+
+from variables import hcl, brooklyn_99_quotes, book_quotes, api_key, base_url
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,23 +16,63 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
-
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
-
 @bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
 async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
     response = random.choice(brooklyn_99_quotes)
     await ctx.send(response)
 
+@bot.command(name='bkqt',help='Sends you a quote from various books')
+async def bok_quotes(ctx):
+    response = random.choice(book_quotes)
+    await ctx.send(response)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+@bot.command(name='haechan', help = 'Talking like Haechan.')
+async def hclee(ctx):
+    response = random.choice(hcl)
+    await ctx.send(response)
+
+@bot.command(name='weather', help = 'Tell the weather with !weather [city name]')
+async def weather(ctx, *, city: str):
+    city_name = city
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    channel = ctx.message.channel
+    if x["cod"] != "404":
+        async with channel.typing():
+            y = x["main"]
+            current_temperature = y["temp"]
+            current_temperature_celsiuis = str(round(current_temperature - 273.15))
+            current_pressure = y["pressure"]
+            current_humidity = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]
+            embed = discord.Embed(title=f"Weather in {city_name}",
+                                  color=ctx.guild.me.top_role.color,
+                                  timestamp=ctx.message.created_at, )
+            embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
+            embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+            embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
+            embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await channel.send(embed=embed)
+    else:
+            await channel.send("City not found.")
+
+
 bot.run(TOKEN)
+
+
+
---
+++ bot/variables.py
@@ -0,0 +1,29 @@
+hcl = ["haechanie wattayo!",
+       "ah~Canada!",
+       "NCT Dream The Love",
+       "I wanna be a billionaire",
+       "Markeu Lee",
+       "Main and visual",
+       "Booming system oh oh",
+       ]
+
+brooklyn_99_quotes = [
+    'I\'m the human form of the 💯 emoji.',
+    'Bingpot!',
+    (
+        'Cool. Cool cool cool cool cool cool cool, '
+        'no doubt no doubt no doubt no doubt.'
+    ),
+    'You are the bomb. Your butt is the bomb',
+
+]
+
+book_quotes = [
+    'Come back home',
+    "the leaven which animated my existence is gone: the charm which cheered me in the gloom of night, and aroused me from my morning slumbers, is forever fled.",
+    'all the knowledge i possess every one else can acquire, but my heart is exclusively my own.',
+    'i posses so much, but my love for her absorbs it all. i possess so much, but without her i have nothing.',
+               ]
+
+api_key = "90f474713aa45debc0fa24a5a68ec392"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
